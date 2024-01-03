88c4a590edb16317a17beda6bf357aa9d0db8373
tetta872 <154493983+tetta872@users.noreply.github.com>
2024-01-03 14:30:51 +0800

    Update 15.login.php

--- 15.login.php
+++ 15.login.php
@@ -1,6 +1,8 @@
 <?php
-    if ($_POST["id"]=="tony" && $_POST["pwd"]=="1234")
-        echo "login success";
-    else
-        echo "login fail";
+    if ($_POST["id"] == "tony" && $_POST["pwd"] == "1234") {
+        echo "歡迎登入，Tony！"; // 登入成功的訊息
+    } else {
+        echo "登入失敗，請檢查帳號和密碼。"; // 登入失敗的訊息
+    }
 ?>
+
